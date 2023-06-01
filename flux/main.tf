d00d64f64e413a2a5a51cfbc9c9452ee88cb87d5
patrickkenney9801 <patrickkenney9801@gmail.com>
2023-06-01 16:13:27 -0500

    Initialize repo

---
+++ flux/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">=1.1.5"
+
+  required_providers {
+    flux = {
+      source = "fluxcd/flux"
+    }
+    github = {
+      source  = "integrations/github"
+      version = ">=5.18.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+  }
+}
